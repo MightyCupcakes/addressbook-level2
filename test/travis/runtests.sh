6a19a4791358f555d2ae6af3bc828221e18615b2
MightyCupcakes <gohydj2003@gmail.com>
2017-01-31 14:00:13 +0800

    test

---
+++ test/travis/runtests.sh
@@ -0,0 +1,11 @@
+# run the program, feed commands from input.txt file and redirect the output to the actual.txt
+java -classpath ../bin seedu.addressbook.Main < input.txt > actual.txt
+
+# compare the output to the expected output
+diff actual.txt expected.txt
+if [ $? -eq 0 ]
+then
+    echo "Test result: PASSED"
+else
+    echo "Test result: FAILED"
+fi
